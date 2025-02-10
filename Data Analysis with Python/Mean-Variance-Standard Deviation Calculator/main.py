b7022639454113f87dcf75f1e9017349e256ba93
punyawat <punyawat.pungphomin@gmail.com>
2025-02-10 20:23:43 +0700

    complete mvs

--- Data Analysis with Python/Mean-Variance-Standard Deviation Calculator/main.py
+++ Data Analysis with Python/Mean-Variance-Standard Deviation Calculator/main.py
@@ -3,7 +3,11 @@ import mean_var_std
 import pprint
 from unittest import main
 
-pprint.pprint(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
+result = mean_var_std.calculate([0,1,2,3,4,5,6,7,8])
+print("\n\n")
+print(">>>mean_var_std.calculate([0,1,2,3,4,5,6,7,8])\n")
+pprint.pprint(result,indent=4,depth=3)
+print("\n\n")
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
